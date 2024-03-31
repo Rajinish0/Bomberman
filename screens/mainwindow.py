77faed286645277bbf659cbab4f198fde14784a8
Olta <oltaabytyqi@gmail.com>
2024-03-31 04:06:33 +0200

    Changed background picture

--- screens/mainwindow.py
+++ screens/mainwindow.py
@@ -13,9 +13,10 @@ implement Map and Control menu screens
 class MainWindow(Screen):
 	def __init__(self):
 		self.MapMenu = Button(W/2, H/2 - 150, 50, 50, text="Maps", textColor=WHITE)
-		self.CntrlMenu = Button(W/2, H/2 - 50, 50, 50, text="Controls", textColor=WHITE)
+		self.CntrlMenu = Button(W/2, H/2 - 50, 50, 50, text="Controls", textColor=WHITE,
+								callBack = lambda: self.gameMgr.setState(CONTROLS_WINDOW))
 		self.LvlEditor = Button(W/2, H/2 + 50, 50, 50, text="Editor", textColor=WHITE,
-						  	 	callBack= lambda: self.gameMgr.setState(LEVEL_EDITOR) )
+						  	 	callBack= lambda: self.gameMgr.setState(LEVEL_EDITOR))
 		self.ExitButton = Button(W/2, H/2 + 150, 50, 50, text="Exit", textColor=WHITE,
 						   		 callBack=lambda: pygame.quit())
 	
@@ -26,7 +27,16 @@ class MainWindow(Screen):
 		self.ExitButton.update()
 	
 	def draw(self, display):
-		display.fill((0, 0, 255))
+		display.fill((255, 255, 255))
+
+		background_image = pygame.image.load('sprites/background.png')
+		scaled_image = pygame.transform.scale(background_image, (W, H))
+
+		alpha_value_bg = 200
+		scaled_image.set_alpha(alpha_value_bg)
+
+		display.blit(scaled_image, (0, 0))
+
 		self.MapMenu.draw(display)
 		self.CntrlMenu.draw(display)
 		self.LvlEditor.draw(display)

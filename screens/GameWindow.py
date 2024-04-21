0e591715af80261c05086f527c005bc8c0bb7bce
rajinish0 <rajinishaneel@gmail.com>
2024-04-21 12:41:23 +0200

    Add game info

--- screens/GameWindow.py
+++ screens/GameWindow.py
@@ -1,7 +1,7 @@
 from screens.screen import Screen
 from constants import *
 from button import Button
-import pygame
+import pygame, os
 from fileReading import GridReader
 from gameObjects.wall import Wall
 from gameObjects.box import Box
@@ -18,7 +18,10 @@ class GameWindow(Screen):
         self.boxWidth = (800 - self.offSetX) // NUM_BOXES
         self.boxHeight = (520 - self.offSetY) // NUM_BOXES
         self.level = GameLevel(file_path,self.boxWidth,self.boxHeight)
-        self.gamesurface = pygame.Surface((800-self.offSetX, 520-self.offSetY))
+        self.gamesurface = pygame.Surface((800-self.offSetX, 520-self.offSetY), pygame.SRCALPHA)
+        self.infosurface = pygame.Surface((800-self.offSetX, (H-520+70)) )
+        self.pl1Image = self.imgHandler.load( os.path.join(IMG_PATH, 'players', 'g1.png'), (50, 70) )
+        self.pl2Image = self.imgHandler.load( os.path.join(IMG_PATH, 'players', 'g2.png'), (50, 70) )
 
 
         self.backButton = Button(
@@ -39,8 +42,11 @@ class GameWindow(Screen):
     def draw(self, display):
         display.fill((110, 161, 100))
         self.gamesurface.fill((110, 161, 100))
+        self.infosurface.fill((110, 161, 100))
+        self.infosurface.blit(self.pl1Image, (0, 0))
+        self.infosurface.blit(self.pl2Image, ((800-self.offSetX)-50, 0))
         self.backButton.draw(display)
         self.level.draw(self.gamesurface)
+        display.blit(self.infosurface, (98, 0))
         display.blit(self.gamesurface, (98, 70))
-
 

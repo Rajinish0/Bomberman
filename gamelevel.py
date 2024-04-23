850a3111a5521289971926690623650a8e8b43c9
vvv1234512 <viig6679@gmail.com>
2024-04-23 22:26:08 +0200

    edit ghost monster

--- gamelevel.py
+++ gamelevel.py
@@ -111,7 +111,7 @@ class GameLevel:
                 self.startEnd(player)
                 break
 
-        self.gameobjs[row][col] = Wall(Point(col, row), self.bw, self.bh)
+        self.gameobjs[row][col] = BorderWall(Point(col, row), self.bw, self.bh)
 
     def update(self):
         if(not self.gameEnd):
